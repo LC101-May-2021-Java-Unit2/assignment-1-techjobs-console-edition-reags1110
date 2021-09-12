020e8ce0622557359c67b42e54eab8e5cdaaf39c
reags1110 <84357221+reags1110@users.noreply.github.com>
2021-09-12 16:25:09 -0500

    changed test

--- src/main/java/TechJobs.java
+++ src/main/java/TechJobs.java
@@ -121,7 +121,7 @@ public class TechJobs {
     private static void printJobs(ArrayList<HashMap<String, String>> someJobs) {
 
         if (someJobs.isEmpty()) {
-            System.out.println("No Results. Try Again.");
+            System.out.print("No Results. Please Try Again.");
         } else {
             for (HashMap<String, String> job: someJobs) {
                 System.out.println("\n*****");
